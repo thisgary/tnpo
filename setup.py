0f70aef80c7b54264b9af2ea84df62168f2c66a8
thisgary <gary.github@gmail.com>
2021-08-04 13:32:09 +0800

    Version fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='Collatz',
         packages=['collatz'],
-        version='',
+        version='0.0',
         description='The cursed Collatz conjecture library.',
         author='thisgary',
         license='MIT',
