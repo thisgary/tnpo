ffbaad66a5ac4ac8746757b6111ffbfc64b02baa
thisgary <gary.github@gmail.com>
2021-08-04 13:26:11 +0800

    Collaz

---
+++ collatz.py
@@ -0,0 +1,36 @@
+# Common math functions used in Collatz conjecture
+''' y = x^n '''
+def is_power(y, x=2):
+    while y%x == 0: y //= x
+    return y == 1
+
+''' y = mx + c '''
+def linear(x, m=3, c=1):
+    return m*x + c
+
+# Collatz Function
+'''
+f(x) = {x//2, x%2=0; 
+        3x+1, x%2=1}
+
+x: Any positive integer
+
+If x is even, divide by 2,
+if x is odd, 3x + 1.
+'''
+def collatz_function(x):
+    return x/2 if x%2 == 0 else linear(x)
+
+# Collatz Conjecture
+def collatz_conjecture(x):
+    step, steps, hike, peak = [], 0, 0, 0
+    while x != 1:
+        y = collatz_function(x)
+        step.append(y)
+        steps += 1
+        if y > peak:
+            hike = steps
+            peak = y
+        x = y
+    return step, steps, hike, peak
+
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='Collatz',
         packages=['collatz'],
-        version='0.0',
+        version='',
         description='The cursed Collatz conjecture library.',
         author='thisgary',
         license='MIT',
