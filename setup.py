9eb0aa2353ce7090ebe4dafb93ded8431a436372
thisgary <gary.github@gmail.com>
2021-08-04 12:35:44 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='Collatz',
+        packages=['collatz'],
+        version='0.0',
+        description='The cursed Collatz conjecture library.',
+        author='thisgary',
+        license='MIT',
+        install_requires=[],
+        setup_requires=['pytest_runner'],
+        test_require=['pytest'],
+        test_suite='tests',)
+
