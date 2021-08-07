c47661255c43bea8963e641e4bddc1e6c6ed5cf4
thisgary <gary.github@gmail.com>
2021-08-07 23:59:50 +0800

    Moved description

--- tnpo/__init__.py
+++ tnpo/__init__.py
@@ -1,1 +1,26 @@
 from .func import *
+
+'''
+3n + 1 Problem
+----------------
+Statement
+----------------
+The problem states that for any positive integer, n,
+that perform i times of f(n) will eventually reach 1,
+and stuck in (4, 2, 1) loop afterward.
+
+----------------
+f(n) function
+----------------
+Given function f(n), where n is positive integer,
+if n is odd, triple it and add one, else, halve it.
+
+----------------
+a(n) sequence
+----------------
+Given function a(n), while n is not 4, n = f(n).
+If the problem statement is indeed true, 
+this function will eventually stop at some point,
+thus proving the statement through 'bruteforce' method.
+'''
+
