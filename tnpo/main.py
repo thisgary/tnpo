b0a1020ddef1da23a4f69b7985c586840fae953f
thisgary <gary.github@gmail.com>
2021-08-07 17:17:46 +0800

    Bruteforce function

--- tnpo/main.py
+++ tnpo/main.py
@@ -13,29 +13,24 @@ def f(n, s=False):
     return (3*n+1)//m if n%2 else n//2
 
 # Relationship Function
-'''
-parent: n
-child : f'(n)
-
-- All parent has at least 1 child
-- A parent can only have 1 child
-- Odd parent can only have 1 child
-'''
 def R(n, s=False):
     c = {2*n} # Childs
     m = 2 if s else 1
-    if n%(6//m)==4//m:
+    if n%(6//m) == 4//m:
         c.add((m*n-1)//3)
     return c
 
 # Sequence Function
-'''
-a(n) = while n not 1, append f(n)
-'''
 def a(n, s=False):
     l = [n]
-    while n != 1:
+    while n != 4:
         n = f(n, s)
         l.append(n)
     return l
+
+# Sequence bruteforcing
+def A(n):
+    while n != 4:
+        if n % 2: n = 3 * n + 1
+        n //= 2
 
